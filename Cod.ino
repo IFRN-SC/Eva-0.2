ec59d64849df46f293e0f5afe0e1b64af5164bbf
isaacmarlon <isaacmarlondasilvalourenco@gmail.com>
2018-04-20 00:08:40 -0300

    [Cod.ino] Atualizado - Versão Eva-0.1

--- Cod.ino
+++ Cod.ino
@@ -1,20 +1,23 @@
-#include "Setup.h"
 #include "Estrategia.h"
 
-Setup set;
 Estrategia estrategia;
+/*
+  Serão usadas funções deste objeto para
+  a calibração e controle estratégico do robô no 
+  void loop().
+*/
 
 void setup(){
-	Serial.begin(9600);
-	robo.configurar(false);
-	//set.pinarLeds(false);   
-	//set.calibrar(false);
-	set.motoresVelPadrao(40);
-	set.distanciaObstaculo(10);
-	//set.separacaoBrancoPreto();
- 
+	Serial.begin(9600); 
+	// ^    ^ A priori, somente deves saber que isto habilita o uso do Serial
+	robo.configurar(false); // Habilita ou desalibita a garra
+	//estrategia.calibrar(false);
+	/* ^         ^  
+	  Determina se será feita a calibracao
+	  da separação-branco-preto.
+	*/
 }
 
 void loop(){
-	estrategia.iniciar();
+	estrategia.run();
 }
