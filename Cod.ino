33506e51a510f6fb65b9ecc5f4596a664359371f
isaacmarlon <isaacmarlondasilvalourenco@gmail.com>
2018-04-20 00:16:28 -0300

    [Cod.ino] Atualizado - Versão Eva-0.2

--- Cod.ino
+++ Cod.ino
@@ -11,7 +11,7 @@ void setup(){
 	Serial.begin(9600); 
 	// ^    ^ A priori, somente deves saber que isto habilita o uso do Serial
 	robo.configurar(false); // Habilita ou desalibita a garra
-	//estrategia.calibrar(false);
+	estrategia.calibrar(true);
 	/* ^         ^  
 	  Determina se será feita a calibracao
 	  da separação-branco-preto.
