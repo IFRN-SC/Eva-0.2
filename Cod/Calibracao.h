2dfdac876a76d71d21ad396289a72934047da927
isaacmarlon <isaacmarlondasilvalourenco@gmail.com>
2018-04-21 21:04:13 -0300

    Some comments && Consequências do último commit

--- Cod/Calibracao.h
+++ Cod/Calibracao.h
@@ -3,9 +3,17 @@
 
 #include <robo_hardware.h>
 
+//_______________________
 #define NULLPRETO 0.0	
 #define NULLBRANCO 100.0
-// !- Usados para a calibracao "Um-Por-Um"
+/* ?--
+	CONSTANTES usadas para passar "valores nulos" para as funções
+	de leitura: calculeMinimoBranco(x,x,x,x) && calculeMaximoPreto(x,x,x,x).
+
+	Quando for selecionado a calibração individual ou "Um-Por-Um", só será
+	necessário a obtenção dos valores para um sensor. E temos que passar
+	quatro parâmetros nas funções de leitura, usaremos as CONSTANTES para
+	descartar qualquer leitura. */
 
 
 class Calibracao {
@@ -19,6 +27,9 @@ public:
 	float mediaEsq;
 	float mediaDir;
 	float mediaDir2;
+	/* ?--
+		Variáveis que guardam a média
+		de branco_preto para todos os sensores.*/
 	
 private:
 	
@@ -26,30 +37,36 @@ private:
 	float minimoBrancoEsq;
 	float minimoBrancoDir;
 	float minimoBrancoDir2;
+	/* ?--
+		Variáveis que guardarão o mínimo
+		valor lido para branco de cada sensor de refletância.*/
 	
 	float maximoPretoEsq2;
 	float maximoPretoEsq;
 	float maximoPretoDir;
 	float maximoPretoDir2;
-	
-	float mediaEsq2;
-	float mediaEsq;
-	float mediaDir;
-	float mediaDir2;
+	/* ?--
+		Variáveis que guardarão o máximo
+		valores lido para branco de cada sensor de refletância.*/
 
 	//~~~~~~~  MENU CALIBRACAO ~~~~~~~//
 	
 	void menuCalibracao();
+	/* ?--
+		Função inicial. É nela que decidiremos
+		como será feita a calibração.*/
 	void esperarParaLer();
+	/* ?--
+		"Função ferramenta" - 
+		Usada para esperar a leitura do Serial.*/
+
+	char escolha; // Usado para controlar o fluxo nos menus.
 	
-	String tipoCompleto; // Usado para concatenações
-	char escolha;
+	//~~~ FORMA DE PEGAR OS VALORES ~~~//
 	
-	// FORMA DE PEGAR OS VALORES
-	
-	void menuFormaPegarValores(char tipo);
-	void pegarUmPorUm(char tipo);
-	void pegarSimultaneamente(char tipo);
+	void menuFormaPegarValores(String tipo);
+	void pegarUmPorUm(String tipo);
+	void pegarSimultaneamente(String tipo);
 	
 	void calculeMinimoBranco(float valorEsq2,float valorEsq,float valorDir,float valorDir2);
 	void calculeMaximoPreto(float valorEsq2,float valorEsq,float valorDir,float valorDir2);
