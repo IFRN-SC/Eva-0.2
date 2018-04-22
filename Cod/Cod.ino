16d0bdc00bc5ee0f74a19e19a28eb8435b1e2aca
isaacmarlon <isaacmarlondasilvalourenco@gmail.com>
2018-04-22 13:45:37 -0300

    Some comments - Cod.ino

--- Cod/Cod.ino
+++ Cod/Cod.ino
@@ -1,21 +1,24 @@
 #include "Estrategia.h"
 
 Estrategia estrategia;
-/*
+/* ?--
   Serão usadas funções deste objeto para
   a calibração e controle estratégico do robô no 
-  void loop().
-*/
+  void loop().*/
 
 void setup(){
 	Serial.begin(9600); 
-	// ^    ^ A priori, somente deves saber que isto habilita o uso do Serial
-	robo.configurar(false); // Habilita ou desalibita a garra
+	/* ?--
+		A priori, somente deves saber que isto 
+		habilita o uso do Serial*/
+	robo.configurar(false); 
+	/* ?--
+		Habilita ou desalibita a garra
+		*/
 	estrategia.calibrar(true);
-	/* ^         ^  
+	/* ?--
 	  Determina se será feita a calibracao
-	  da separação-branco-preto.
-	*/
+	  da separação-branco-preto.*/
 }
 
 void loop(){
