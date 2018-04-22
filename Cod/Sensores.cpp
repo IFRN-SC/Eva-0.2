8bf6626039973d1713460410c06ab3bdd2e8b28a
isaacmarlon <isaacmarlondasilvalourenco@gmail.com>
2018-04-22 14:31:00 -0300

    Added funções de calibração - Sensores.cpp

--- Cod/Sensores.cpp
+++ Cod/Sensores.cpp
@@ -207,3 +207,21 @@ bool Sensores::maisDirViuBranco() {
 bool Sensores::maisDirViuPreto() {
 	return (robo.lerSensorLinhaDir2() < 54);  
 } 
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//~~~~~~~~~~~~~~~~~~~~~~~~~ CALIBRAÇÃO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+void Sensores::setMinimoBranco(float valor_lido) {
+	if (valor_lido < minimoBranco) minimoBranco = valor_lido;
+}
+
+void Sensores::setMaximoPreto(float valor_lido) {
+	if (valor_lido > maximoPreto) maximoPreto = valor_lido; 
+}
+
+void Sensores::calculeMedia(float minimoBranco,float maximoPreto) {
+	media = ((minimoBranco + maximoPreto)/6);
+}
+
+float Sensores::getSeparacao() { return media; }
